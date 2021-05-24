b0beb9179fda1bbae56f497ae7f6f3de509c2935
alaamhna3354 <alaamhna3354@gmail.com>
2021-05-25 01:49:03 +0300

    modifile route

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ Vue.use(VueRouter);
 const routes = [
     /* dashboard */
     {
-        path: '/',
+        path: '/admin',
         name: 'Adminbody',
         component: () => import('../components/body'),
     },
