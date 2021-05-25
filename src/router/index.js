820c270dae24ecbff371094fd8250663b45b4441
rahaf-jamool <rahafjamool1@gmail.com>
2021-05-25 14:11:23 +0300

    add dash

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ Vue.use(VueRouter);
 const routes = [
     /* dashboard */
     {
-        path: '/',
+        path: '/Admin',
         name: 'Adminbody',
         component: () => import('../components/body'),
     },
